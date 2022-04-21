61c25d81d131965568b1e7e345e76e181b3b5bcd
zhaow3w <394772311@qq.com>
2022-04-21 15:48:16 +0800

    helloworld

---
+++ hello/src/main/java/com/example/demo/web/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+	 @GetMapping("/hello")
+	    public String hello(){
+	        return "springboot hello world...";
+	    }
+}
